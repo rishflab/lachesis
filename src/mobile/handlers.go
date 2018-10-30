4172fabea9dcfc8be100d863e356912390a5a3d3
rishflab <rishflab@hotmail.com>
2018-10-30 13:48:41 +1100

    added interface documentation

--- src/mobile/handlers.go
+++ src/mobile/handlers.go
@@ -5,12 +5,13 @@ These types are exported and need to be implemented and used by the mobile
 application.
 */
 
-//------------------------------------------------------------------------------
-
+// CommitHandler handles retreival of the state hash.
+// What the purpose of state hash retrieval for the client application?
 type CommitHandler interface {
 	OnCommit([]byte) []byte
 }
 
+// Handles mobile app mobile app exceptions.
 type ExceptionHandler interface {
 	OnException(string)
 }
--- src/net/client_socket.go
+++ src/net/client_socket.go
@@ -1,9 +1,11 @@
 package net
 
+// A listener 
 type MessageListener interface {
 	OnMessage(msg []byte, err error)
 }
 
+// ClientSocket represents a client socket connection
 type ClientSocket interface {
 	Connect(addr string, path string) error
 	AddMessageListener(listener MessageListener)
