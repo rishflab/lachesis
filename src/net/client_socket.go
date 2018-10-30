6fa562cfabf68f4a78483c33d76b18b90db4ef27
rishflab <rishflab@hotmail.com>
2018-10-30 13:52:05 +1100

    removed some mistakes from comments

--- src/net/client_socket.go
+++ src/net/client_socket.go
@@ -1,11 +1,13 @@
 package net
 
+// Message handler for server messages. Concrete implementations could
+// be created using websockets, in-memory transport, UDP etc.
 type MessageListener interface {
 	OnMessage(msg []byte, err error)
 }
 
 // ClientSocket represents a client socket connection
-type ClientSocket interface {s
+type ClientSocket interface {
 	Connect(addr string, path string) error
 	AddMessageListener(listener MessageListener)
 	SendMessage(msg []byte) error
