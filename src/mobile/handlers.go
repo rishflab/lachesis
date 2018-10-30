9fc7cb11edc4eb3393e8db43ddd3bf463cf024a6
rishflab <rishflab@hotmail.com>
2018-10-30 13:52:05 +1100

    removed some mistakes from comments

--- src/mobile/handlers.go
+++ src/mobile/handlers.go
@@ -5,8 +5,9 @@ These types are exported and need to be implemented and used by the mobile
 application.
 */
 
-// CommitHandler handles retreival of the state hash.
-// What the purpose of state hash retrieval for the client application?
+// CommitHandler is called when Lachesis has committed a block to the DAG and publishes
+// that message to the mobile app. It returns the state hash resulting from applying 
+// the block's transactions to the state.
 type CommitHandler interface {
 	OnCommit([]byte) []byte
 }
--- src/net/client_socket.go
+++ src/net/client_socket.go
@@ -1,12 +1,13 @@
 package net
 
-// A listener 
+// Message handler for server messages. Concrete implementations could
+// be created using websockets, in-memory transport, UDP etc.
 type MessageListener interface {
 	OnMessage(msg []byte, err error)
 }
 
 // ClientSocket represents a client socket connection
-type ClientSocket interface {
+type ClientSocket interface {s
 	Connect(addr string, path string) error
 	AddMessageListener(listener MessageListener)
 	SendMessage(msg []byte) error
